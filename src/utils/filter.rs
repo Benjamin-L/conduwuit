48cdf483894274a0809feafb5238e6e3d535e514
Benjamin Lee <benjamin@computer.surgery>
2024-05-21 14:18:51 -0700

    implement room.account_data.(not_)rooms filter on /sync

--- src/utils/filter.rs
+++ src/utils/filter.rs
@@ -136,6 +136,7 @@ pub(crate) struct CompiledFilterDefinition<'a> {
 
 pub(crate) struct CompiledRoomFilter<'a> {
 	rooms: AllowDenyList<'a, RoomId>,
+	pub(crate) account_data: CompiledRoomEventFilter<'a>,
 	pub(crate) timeline: CompiledRoomEventFilter<'a>,
 	pub(crate) ephemeral: CompiledRoomEventFilter<'a>,
 	pub(crate) state: CompiledRoomEventFilter<'a>,
@@ -168,6 +169,7 @@ impl<'a> TryFrom<&'a RoomFilter> for CompiledRoomFilter<'a> {
 			// TODO: consider calculating the intersection of room filters in
 			// all of the sub-filters
 			rooms: AllowDenyList::from_slices(source.rooms.as_deref(), &source.not_rooms),
+			account_data: (&source.account_data).try_into()?,
 			timeline: (&source.timeline).try_into()?,
 			ephemeral: (&source.ephemeral).try_into()?,
 			state: (&source.state).try_into()?,
